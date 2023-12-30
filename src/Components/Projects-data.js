fc28fa80577a78c0ee6de1b1a3cdfbec0ad56c7c
srvinith <r36041595@gmail.com>
2023-12-30 20:10:05 +0530

    new

--- src/Components/Projects-data.js
+++ src/Components/Projects-data.js
@@ -7,19 +7,21 @@ const Projects=[
         img:onwords,
         Name:'Onwords',
         desc:`I'm  a frontend web developer fueled by a passion for crafting funky and interactive digital experiences. I thrive on translating ideas into captivating visuals and seamless user interactions using a blend of HTML, CSS, and JavaScript wizardry.   My goal? To sprinkle a touch of excitement and creativity into every project, `,
-        link:'https://onwords.in/',
+        links:'https://onwords.in/',
     },
     {
         id:2,
         img:pscastle,
         Name:'PS Castle',
-        desc:`I'm  a frontend web developer fueled by a passion for crafting funky and interactive digital experiences. I thrive on translating ideas into captivating visuals and seamless user interactions using a blend of HTML, CSS, and JavaScript wizardry.   My goal? To sprinkle a touch of excitement and creativity into every project, `
+        desc:`I'm  a frontend web developer fueled by a passion for crafting funky and interactive digital experiences. I thrive on translating ideas into captivating visuals and seamless user interactions using a blend of HTML, CSS, and JavaScript wizardry.   My goal? To sprinkle a touch of excitement and creativity into every project, `,
+        // links:'https://onwords.in/',
     },
     {
         id:3,
         img:foodon,
         Name:'Food On website',
-        desc:`I'm  a frontend web developer fueled by a passion for crafting funky and interactive digital experiences. I thrive on translating ideas into captivating visuals and seamless user interactions using a blend of HTML, CSS, and JavaScript wizardry.   My goal? To sprinkle a touch of excitement and creativity into every project, `
+        desc:`I'm  a frontend web developer fueled by a passion for crafting funky and interactive digital experiences. I thrive on translating ideas into captivating visuals and seamless user interactions using a blend of HTML, CSS, and JavaScript wizardry.   My goal? To sprinkle a touch of excitement and creativity into every project, `,
+        // links:'https://onwords.in/',
     },
 
 ]
